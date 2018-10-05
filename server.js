719dbd6eece7424983140ce4baf3cd24077c853c
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-05 11:11:35 +0200

    add runServe and closeServer functions and export them as promises

--- server.js
+++ server.js
@@ -16,9 +16,46 @@ app.use(morgan('common'));
 // Route the requests to the appropriate router module
 app.use('/blog-posts', postsRouter);
 
-// Open the server port 8080
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`The process started listening at port ${process.env.PORT || 8080}`);
-});
+//  function for running the server. returns a promise to prevent a race condition
+// when running tests
+
+let server;
+
+function runServer() {
+  const PORT = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(PORT, () => {
+      console.log(`The proces started listening at ${PORT}`);
+      resolve(server);
+    }).on('error', (err) => {
+      console.error(`Starting the server produced an error ${err} while starting`);
+      reject(err);
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('The server is closing.');
+    server.close(function(err) {
+      if(err) {
+        console.error(`Closing the server produced an error ${err}`);
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
+// Add the ability to start the server if the script is called from the commandline
+
+if(require.main === module) {
+  runServer().catch(err => {console.error(err)});
+}
+
+module.exports = {app, runServer, closeServer};
+
 
 
